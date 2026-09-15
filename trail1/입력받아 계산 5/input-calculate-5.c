ea5de054a3b54046e10cfeaeaf54f471d94d7e3e
DALTW <ppong722@naver.com>
2026-09-15 09:25:40 +0900

    [Passed] 입력받아 계산 5 (2ms, 0MB)

---
+++ trail1/입력받아 계산 5/input-calculate-5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a <= 100 & a >= 1 & b <= 100 & b >= 1)
+    {
+        printf("%d",a+b);
+    }
+    return 0;
+}
