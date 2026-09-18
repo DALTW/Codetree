994cac3daf4470ec8e5d798e96c2a78cfb3b55fc
DALTW <ppong722@naver.com>
2026-09-18 16:01:34 +0900

    [Passed] 큰 수에서 빼기 (1ms, 0MB)

---
+++ trail1/큰 수에서 빼기/subtract-from-large-number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int A,B;
+    scanf("%d %d",&A,&B);
+
+    if(A > B)
+    {
+        printf("%d",A-B);
+    }
+    else
+    {
+        printf("%d",B-A);
+    }
+    return 0;
+}
