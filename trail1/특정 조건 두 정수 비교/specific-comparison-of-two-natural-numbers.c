8a366e137c767d8407ac3b3adc9cf7b32b6c2f91
DALTW <ppong722@naver.com>
2026-09-20 11:57:20 +0900

    [Passed] 특정 조건 두 정수 비교 (1ms, 0MB)

---
+++ trail1/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int A,B;
+    scanf("%d %d",&A,&B);
+
+    if(A<B)
+    {
+        printf("1 ");
+    }
+    else
+    {
+        printf("0 ");
+    }
+
+    if(A==B)
+    {
+        printf("1");
+    }
+    else
+    {
+        printf("0");
+    }
+    return 0;
+}
