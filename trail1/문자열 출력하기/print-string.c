c4d5c19cc78b8146046b116a825b5646bf6bcc94
DALTW <ppong722@naver.com>
2026-09-25 09:37:56 +0900

    [Passed] 문자열 출력하기 (2ms, 0MB)

---
+++ trail1/문자열 출력하기/print-string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d",&N);
+    char c[20] = "LeebrosCode";
+
+    for(int i = 0; i < N; i++)
+    {
+        printf("%s\n",c);
+    }
+
+    return 0;
+}
