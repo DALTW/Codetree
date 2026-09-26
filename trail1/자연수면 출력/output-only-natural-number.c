acb1c3459beafd6b0fe6310c29f669e7a92f6306
DALTW <ppong722@naver.com>
2026-09-26 20:10:39 +0900

    [Passed] 자연수면 출력 (1ms, 0MB)

---
+++ trail1/자연수면 출력/output-only-natural-number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int A,B;
+    scanf("%d %d",&A,&B);
+
+    if(A > 0)
+    {
+        for(int i = 0; i < B; i++)
+        {
+            printf("%d",A);
+        }
+    }
+    else
+    {
+        printf("0");
+    }
+    return 0;
+}
